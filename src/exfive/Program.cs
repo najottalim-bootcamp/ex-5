eccb664ae7a961e46e7157238946f0ab7329681e
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-03 21:28:28 +0500

    the end

--- src/exfive/Program.cs
+++ src/exfive/Program.cs
@@ -1,3 +1,22 @@
-﻿Console.WriteLine("Hello, Bahriddin!");
+﻿using exfive.Data;
+using exfive.Models;
+using exfive.Repository;
+using Npgsql.Internal;
+
+Console.WriteLine("Hello, Bahriddin!");
 
 
+User user = new User()
+{
+    UserId = 1,
+    FirsName = "Bahriddin",
+    LastName = "Abdusalomov",
+    Email = "bahriddin@gmail.com",
+    UserName = "bahriddin01",
+    Password = "bahriddin0404"
+};
+
+UserRepository userRepository = new UserRepository(new AppDbContext());
+
+bool result = await userRepository.CreateAsync(user);
+Console.WriteLine(result);
