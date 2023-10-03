87812d87f2353af1b3dc6c9105e63610dd9a8b54
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-10-03 21:01:34 +0500

    added methods in interface

--- src/exfive/Interfaces/Users/IUserRepository.cs
+++ src/exfive/Interfaces/Users/IUserRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace exfive.Interfaces.Users;
+﻿using exfive.Models;
+
+namespace exfive.Interfaces.Users;
 
 public interface IUserRepository
 {
-    //public Task<User> GetById(int id);
+    public Task<User> GetByIdAsync(int id);
+
+    public Task<bool> GelAllAsnyc();
+
+    public Task<bool> CreateAsync(User user);
+
+    public Task<bool> UpdateAsync(User user);
+
+    public Task<bool> DeleteAsync(int id);
 }
